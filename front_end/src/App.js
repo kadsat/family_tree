889435b326002860a56124c2662e42ee584d4f3f
Satiesh Arun Raj <kadsat@gmail.com>
2023-03-27 17:42:41 -0400

    improvisation

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,29 @@
 //import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function Person(props) {
   const [isClicked, setClicked] = useState(false);
-  function handleClick() {
-    setClicked(true);
-  }
   let name   = props.state.name;
   let spouse = props.state.spouse;
+  //{isClicked? <Person state={props.state.children[0]}/>: null}
   return (
-    <>
+    <div className='App'>
       <p>{name}</p>
       <p>{spouse}</p>
-      <button onClick={handleClick}>children</button>
-      <div style={{textIndent: '40px'}}>
-        {isClicked? <Person state={props.state.children[0]}/>: null}
+      <button onClick={() => setClicked(!isClicked)}>children</button>
+      <div style={{textIndent: '40'}}>
+        {
+          isClicked? 
+            props.state.children.map(
+              (child) => <Person state={child}/>
+            )
+          : 
+          null
+        }
       </div>
-    </>
-  ); 
+    </div>
+  );
 }
 
 
